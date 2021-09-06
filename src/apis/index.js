103603bf197ccd4df1bd6d865d853450319d73eb
Sri Vineeth <mrsrv7@yahoo.com>
2021-09-06 19:06:24 +0530

    Articulated the API documentation using Swagger UI

---
+++ src/apis/index.js
@@ -0,0 +1,23 @@
+import { config } from "dotenv-flow";
+import userApiObject from "./userApi";
+
+config();
+
+const swaggerConfig = {
+  openapi: "3.0.0",
+  info: {
+    version: "0.0.1",
+    title: "API Explorer for MEN App using Swagger UI",
+    description:
+      "An Interactive REST API Explorer for MEN App using Swagger UI",
+  },
+  servers: [
+    {
+      url: "http://localhost:6116",
+      description: process.env.DESCRIPTION,
+    },
+  ],
+  paths: { ...userApiObject },
+};
+
+export default swaggerConfig;
